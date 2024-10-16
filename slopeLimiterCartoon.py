4d4b6a27610dbfd0166aabec628c8ef3d007b520
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2024-10-16 08:18:39 -0400

    updates

--- slopeLimiterCartoon.py
+++ slopeLimiterCartoon.py
@@ -8,24 +8,15 @@ plt.style.use( 'publication.sty' )
 from gaussxw import gaussxw
 from dgUtilities import *
 
-xL = 0.4
-xH = 1.0
-xExact = np.linspace( xL, xH, 100 )
-x      = np.linspace( xL, xH, 3 )
-dx     = ( x[-1] - x[0] ) / np.float64( x.shape[0] )
+def uExact( x ):
+    return np.tanh((x-0.05)/0.1)
 
-def rhoExact( x ):
-    return 1.0 + 0.1 * np.sin( 3.1 * np.pi * x )
+def plotBase( fig, ax ):
 
-def plotBase( fig, ax, vmin, vmax ):
+    ax.set_xlim( xL - 0.1, xH + 0.1 )
 
-#    ax.set_xlim( xL - 0.1, xH + 0.1 )
-#    ax.set_ylim( vmin, vmax )
-
-    ax.axvline( 0.4, c = 'k' )
-    ax.axvline( 0.6, c = 'k' )
-    ax.axvline( 0.8, c = 'k' )
-    ax.axvline( 1.0, c = 'k' )
+    for i in range( x.shape[0]+1 ):
+        ax.axvline( xL+i*dx, c = 'k' )
 
     ax.axis( 'off' )
 
@@ -48,16 +39,35 @@ def nodalToModal( N, xl ):
 
     for i in range( N ):
         intU[i] \
-          = np.sum( wqNN * rhoExact( xqNN ) * Lagrange( etaqNN, etaqN, i ) )
+          = np.sum( wqNN * uExact( xqNN ) * Lagrange( etaqNN, etaqN, i ) )
 
-    rho_q = np.dot( np.linalg.inv( M ), intU )
+    u_q = np.dot( np.linalg.inv( M ), intU )
 
     for n in range( N ):
         Cn[n] \
-          = np.sum( wqNN * rhoh( etaqNN, rho_q, etaqN ) \
+          = np.sum( wqNN * uh( etaqNN, u_q, etaqN ) \
                       * Legendre( etaqNN, n ) )
 
     return Cn
+
+def plotExactSolution( fig, ax, ls1, c1, ls2, c2, ls3, c3 ):
+
+    xl = xL
+    xh = xl + dx
+    xx = np.linspace( xl, xh, 100 )
+    l1 = ax.plot( xx, uExact( xx ), ls1, color = c1 )
+
+    xl += dx
+    xh += dx
+    xx = np.linspace( xl, xh, 100 )
+    l2 = ax.plot( xx, uExact( xx ), ls2, color = c2 )
+
+    xl += dx
+    xh += dx
+    xx = np.linspace( xl, xh, 100 )
+    l3 = ax.plot( xx, uExact( xx ), ls3, color = c3 )
+
+    return# l1, l2, l3
 
 def plotDensity( fig, ax, c, N, iX1 ):
 
@@ -79,47 +89,56 @@ def plotDensity( fig, ax, c, N, iX1 ):
 
     for i in range( N ):
         intU[i] \
-          = np.sum( wqNN * rhoExact( xqNN ) * Lagrange( etaqNN, etaqN, i ) )
+          = np.sum( wqNN * uExact( xqNN ) * Lagrange( etaqNN, etaqN, i ) )
 
-    rho_q = np.dot( np.linalg.inv( M ), intU )
+    u_q = np.dot( np.linalg.inv( M ), intU )
 
     for i in range( N ):
         Cn[i] \
-          = np.sum( wqNN * rhoh( xqNN, rho_q, xqN ) * Legendre( etaqNN, i ) )
+          = np.sum( wqNN * uh( xqNN, u_q, xqN ) * Legendre( etaqNN, i ) )
 
     xx = np.linspace( xl, xh, 100 )
-    rho = rhoh( xx, rho_q, xqN )
+    u = uh( xx, u_q, xqN )
 
-    ax.plot( xx , rho * np.ones( xx.shape[0], dtype = np.float64 ), \
+    ax.plot( xx , u * np.ones( xx.shape[0], dtype = np.float64 ), \
              '-', color = c )
-
-    if ( iX1 == 1 ) :
-        ax.plot( xx , rhoExact( xx ), '-', color = 'k' )
-    else :
-        ax.plot( xx , rhoExact( xx ), '-', color = 'k' )
 
     xl += dx
     xh += dx
 
-    return rho_q
+    return u_q
 
-vmin = 0.95 * rhoExact( x ).min()
-vmax = 1.04 * rhoExact( x ).max()
+if ( __name__ == '__main__' ) :
 
-fig, ax = plt.subplots()
-plotBase( fig, ax, vmin, vmax )
-C1m = plotDensity( fig, ax, 'r', 1, 0 )
-C1  = plotDensity( fig, ax, 'm', 1, 1 )
-C1p = plotDensity( fig, ax, 'b', 1, 2 )
-ax.plot( [xL+0.5*dx,xL+1.5*dx], [C1m,C1], 'r--' )
-ax.plot( [xL+1.5*dx,xL+2.5*dx], [C1,C1p], 'b--' )
+    xL = -0.3
+    xH = 0.3
+    x  = np.linspace( xL, xH, 3 )
+    dx = ( x[-1] - x[0] ) / np.float64( x.shape[0] )
 
-N = 3
-Cn = nodalToModal( N, xL+dx )
-eta = np.linspace( -0.5, +0.5, 10 )
-y = sum( [ Cn[i] * Legendre( eta, i ) for i in range( N ) ] )
-xx = np.linspace( xL+dx, xL+2*dx, 10 )
-ax.plot( xx, Cn[0] + Cn[1] * Legendre( eta, 1 ), 'm' )
+    fig, ax = plt.subplots()
+    plotBase( fig, ax )
+    plotExactSolution( fig, ax, '-', 'k', '-', 'k', '-', 'k' )
+    plt.show()
+    plt.close()
 
-plt.show()
-plt.close()
+    fig, ax = plt.subplots()
+    plotBase( fig, ax )
+    plotExactSolution( fig, ax, '-', 'r', '-', 'm', '-', 'b' )
+    plt.show()
+    plt.close()
+
+    #C1m = plotDensity( fig, ax, 'r', 1, 0 )
+    #C1  = plotDensity( fig, ax, 'm', 1, 1 )
+    #C1p = plotDensity( fig, ax, 'b', 1, 2 )
+    #ax.plot( [xL+0.5*dx,xL+1.5*dx], [C1m,C1], 'r--' )
+    #ax.plot( [xL+1.5*dx,xL+2.5*dx], [C1,C1p], 'b--' )
+    #
+    #N = 3
+    #Cn = nodalToModal( N, xL+dx )
+    #eta = np.linspace( -0.5, +0.5, 10 )
+    #y = sum( [ Cn[i] * Legendre( eta, i ) for i in range( N ) ] )
+    #xx = np.linspace( xL+dx, xL+2*dx, 10 )
+    #ax.plot( xx, Cn[0] + Cn[1] * Legendre( eta, 1 ), 'm' )
+
+    plt.show()
+    plt.close()
